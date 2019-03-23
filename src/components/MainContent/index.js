a3ff3608ec35e11a8f989f5250f3b24a3597ee15
cnrdWright <conrad.wright@trineo.com>
2019-03-24 11:25:47 +1300

    application infrastructure stuff

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './mainContent.css'
 
+import BookCard from './partials/BookCard'
+
 export default class MainContentBlock extends React.Component {
     render() {
         return (
             <div className="mainContent">
-                <h2 className="main-content-header">Main Content Block</h2>
+                <h2 className="main-content-header">On The Shelve</h2>
                 <p>Some Content will eventually go here</p>
+                <BookCard books={this.props} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Root from './Root'
+import { BrowserRouter, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Root>
+        <BrowserRouter >
+            <Route path="/" component={App}/>
+        </BrowserRouter>
+    </Root>, document.getElementById('root')
+    );
